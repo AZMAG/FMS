544960325d99e965d7704491bf11482d5e1e12b4
Jack Fairfield <jfairfield@azmag.gov>
2019-12-30 16:16:48 -0700

    Created the MVC project.  Added Menu

---
+++ FMS Dashboard/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FMS_Dashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        public ActionResult Corridors()
+        {
+            return View();
+        }
+
+        public ActionResult QueryBuilder()
+        {
+            return View();
+        }
+    }
+}
---
+++ FMS Dashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FMS_Dashboard.MvcApplication" Language="C#" %>
