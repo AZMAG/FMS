c0a0cd58c91cb999c685b3f14e959a38e8433b74
Jack Fairfield <jfairfield@azmag.gov>
2022-09-01 16:12:52 -0700

    Match old navbar style.  Add routes for future pages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import HomePage from './pages/Home';
-import DetectorPage from './pages/Detector';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import { DataProvider } from './stores/DataContext';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Home";
+import DetectorPage from "./pages/Detector";
+import CorridorsPage from "./pages/Corridors";
+import QueryBuilderPage from "./pages/QueryBuilder";
+import AdminPage from "./pages/Admin";
+
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { DataProvider } from "./stores/DataContext";
 
 function App() {
-  return (
-    <div className="w-full h-screen">
-      <DataProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="detector/:id" element={<DetectorPage />} />
-            <Route exact path="/" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </DataProvider>
-    </div>
-  );
+    return (
+        <div className="w-full h-screen">
+            <DataProvider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route exact path="/" element={<HomePage />} />
+                        <Route path="detector/:id" element={<DetectorPage />} />
+                        <Route path="corridors" element={<CorridorsPage />} />
+                        <Route path="query" element={<QueryBuilderPage />} />
+                        <Route path="admin" element={<AdminPage />} />
+                    </Routes>
+                </Router>
+            </DataProvider>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,67 +1,58 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../Logo.png';
+import React from "react";
+import logo from "../Logo.png";
+import { useHref, useNavigate, useLocation } from "react-router-dom";
 
 export default function CustomNavbar() {
-  const pages = ['Home', 'Corridors', 'Query Builder', 'Admin'];
-  return (
-    <>
-      <nav className="bg-white border-gray-200 px-1 sm:px-4 py-2.5 ">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-          <a href="https://flowbite.com/" className="flex items-center">
-            <img
-              src={logo}
-              style={{ height: 60 }}
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
-            ></img>
-          </a>
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            {/* <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  About
-                </a>
-              </li>
-              
-            </ul> */}
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+    const links = [
+        { text: "Home", toLink: "/" },
+        { text: "Corridors", toLink: "/corridors" },
+        { text: "Query Builder", toLink: "/query" },
+        { text: "Admin", toLink: "/admin" },
+    ];
+
+    return (
+        <>
+            <nav className="bg-white border-gray-200 px-1 sm:px-4 py-2.5 border-b-stone-900 mb-2">
+                <div className="container flex flex-wrap items-center mx-auto">
+                    <a
+                        href="https://flowbite.com/"
+                        className="flex items-center h-16"
+                    >
+                        <img
+                            src={logo}
+                            className="mr-3 h-full"
+                            alt="FMS Logo"
+                        ></img>
+                    </a>
+                    <div className="flex items-center pl-3">
+                        <ul className="flex">
+                            {links.map((link, i) => {
+                                console.log(pathname);
+                                const isActive = pathname === link.toLink;
+
+                                function linkClicked() {
+                                    navigate(link.toLink);
+                                }
+                                return (
+                                    <li
+                                        onClick={linkClicked}
+                                        key={i}
+                                        className={`bg-gray-400 text-mag-teal font-semibold color block py-2 px-2 mx-1 cursor-pointer hover:underline text-lg ${
+                                            isActive
+                                                ? "bg-opacity-30"
+                                                : "bg-opacity-0 hover:bg-opacity-5"
+                                        }`}
+                                    >
+                                        {link.text}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
