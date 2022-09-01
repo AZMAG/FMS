d31bcc33c25f8d1413ede439dc50a89fb0bca970
vwolfley <vern.wolfley@gmail.com>
2022-09-01 15:58:01 -0700

    update styles for charts pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import HomePage from './pages/Home';
-import DetectorPage from './pages/Detector';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import { DataProvider } from './stores/DataContext';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Home";
+import DetectorPage from "./pages/Detector";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { DataProvider } from "./stores/DataContext";
 
 function App() {
-  return (
-    <div className="w-full h-screen">
-      <DataProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="detector/:id" element={<DetectorPage />} />
-            <Route exact path="/" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </DataProvider>
-    </div>
-  );
+    return (
+        <div className="w-full h-screen">
+            <DataProvider>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route path="detector/:id" element={<DetectorPage />} />
+                        <Route exact path="/" element={<HomePage />} />
+                    </Routes>
+                </Router>
+            </DataProvider>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Detector/Charts/DistributionOfDataPassingQCByDate.js
+++ client/src/components/Detector/Charts/DistributionOfDataPassingQCByDate.js
@@ -1,158 +1,161 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
-  getTimeLabels,
-  getMultipleSeriesByField,
-  sortTimeData,
-} from './chartDataHelpers';
+    getTimeLabels,
+    getMultipleSeriesByField,
+    sortTimeData,
+} from "./chartDataHelpers";
 
-import LineChart from './LineChart';
+import LineChart from "./LineChart";
 
 import {
-  Chart,
-  ChartTitle,
-  ChartSeries,
-  ChartSeriesItem,
-  ChartValueAxis,
-  ChartValueAxisItem,
-  ChartCategoryAxis,
-  ChartCategoryAxisItem,
-  ChartLegend,
-  ChartSeriesItemTooltip,
-  ChartCategoryAxisLabels,
-} from '@progress/kendo-react-charts';
+    Chart,
+    ChartTitle,
+    ChartSeries,
+    ChartSeriesItem,
+    ChartValueAxis,
+    ChartValueAxisItem,
+    ChartCategoryAxis,
+    ChartCategoryAxisItem,
+    ChartLegend,
+    ChartSeriesItemTooltip,
+    ChartCategoryAxisLabels,
+} from "@progress/kendo-react-charts";
+import "hammerjs";
 
 axios.defaults.withCredentials = true;
 
-const chartColors = ['red', 'blue', 'green'];
+const chartColors = ["red", "blue", "green"];
 const font = `bold 12px "Avenir Next W00", "Helvetica Neue", Helvetica, Arial, sans-serif`;
 
 export default function DistributionOfDataPassingQCByDate({ det_num }) {
-  const [data, setData] = useState(null);
-  const [series, setSeries] = useState([]);
-  const [dateLabels, setDateLabels] = useState([]);
+    const [data, setData] = useState(null);
+    const [series, setSeries] = useState([]);
+    const [dateLabels, setDateLabels] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const res = await axios.get(
-        'http://magdevarcgis/fms/Detector/GetErrors',
-        {
-          params: {
-            det_num: 50,
-            year: '2021',
-          },
-        }
-      );
-      let _data = res.data.map((item) => {
-        item.date = new Date(parseInt(item.collected.replace(/[^0-9 +]/g, '')));
-        return item;
-      });
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(
+                "http://magdevarcgis/fms/Detector/GetErrors",
+                {
+                    params: {
+                        det_num: 50,
+                        year: "2021",
+                    },
+                }
+            );
+            let _data = res.data.map((item) => {
+                item.date = new Date(
+                    parseInt(item.collected.replace(/[^0-9 +]/g, ""))
+                );
+                return item;
+            });
 
-      var startDate = new Date('2018-01-01');
-      var endDate = new Date('2018-12-31');
+            var startDate = new Date("2018-01-01");
+            var endDate = new Date("2018-12-31");
 
-      var getDateArray = function (start, end) {
-        var arr = new Array();
-        var dt = new Date(start);
-        while (dt <= end) {
-          const totalDailyErrors = _data.filter((item) => {
-            // console.log(item);
-            return item.date.getDate() === dt.getDate();
-          });
+            var getDateArray = function (start, end) {
+                var arr = new Array();
+                var dt = new Date(start);
+                while (dt <= end) {
+                    const totalDailyErrors = _data.filter((item) => {
+                        // console.log(item);
+                        return item.date.getDate() === dt.getDate();
+                    });
 
-          const min_since = {};
-          totalDailyErrors.forEach((error) => {
-            min_since[error.min_since] = true;
-          });
+                    const min_since = {};
+                    totalDailyErrors.forEach((error) => {
+                        min_since[error.min_since] = true;
+                    });
 
-          let passing = 1 - Object.keys(min_since).length / 288;
+                    let passing = 1 - Object.keys(min_since).length / 288;
 
-          if (passing < 0) {
-            alert('passing is less than 0');
-            passing = 0;
-          }
+                    if (passing < 0) {
+                        alert("passing is less than 0");
+                        passing = 0;
+                    }
 
-          const obj = {
-            date: new Date(dt),
-            passing,
-            month: dt.getMonth(),
-          };
+                    const obj = {
+                        date: new Date(dt),
+                        passing,
+                        month: dt.getMonth(),
+                    };
 
-          arr.push(obj);
-          dt.setDate(dt.getDate() + 1);
-        }
-        return arr;
-      };
-      const seriesData = getDateArray(startDate, endDate);
-      var months = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      ];
+                    arr.push(obj);
+                    dt.setDate(dt.getDate() + 1);
+                }
+                return arr;
+            };
+            const seriesData = getDateArray(startDate, endDate);
+            var months = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ];
 
-      const labels = seriesData.map((ser) => {
-        return months[ser.month];
-      });
-      setDateLabels(labels);
-      setSeries(seriesData);
-    })();
-  }, [det_num]);
-  return (
-    <div style={{ marginRight: '15px', flex: 1 }}>
-      {series.length > 0 ? (
-        <div>
-          <Chart>
-            <ChartTitle text="Distibution of Data Passing Quality Control Criteria by Date" />
-            <ChartValueAxis>
-              <ChartValueAxisItem
-                title={{
-                  text: 'Percent of Data Rows Valid',
-                  font,
-                }}
-                labels={{ format: '{0:p}' }}
-              />
-            </ChartValueAxis>
-            <ChartCategoryAxis>
-              <ChartCategoryAxisItem
-                labels={{
-                  font,
-                  step: 15.5,
-                  skip: 15,
-                  rotation: 'auto',
-                  padding: [0, 0, 0, 120],
-                  visible: true,
-                }}
-                categories={dateLabels}
-              />
-            </ChartCategoryAxis>
-            <ChartSeries>
-              <ChartSeriesItem
-                field="passing"
-                type="area"
-                color={'blue'}
-                data={series}
-                markers={{ visible: false }}
-                tooltip={{
-                  background: 'blue',
-                  visible: true,
-                  format: '{0}',
-                }}
-              />
-            </ChartSeries>
-          </Chart>
+            const labels = seriesData.map((ser) => {
+                return months[ser.month];
+            });
+            setDateLabels(labels);
+            setSeries(seriesData);
+        })();
+    }, [det_num]);
+    return (
+        <div style={{ marginRight: "15px", flex: 1 }}>
+            {series.length > 0 ? (
+                <div>
+                    <Chart>
+                        <ChartTitle text="Distibution of Data Passing Quality Control Criteria by Date" />
+                        <ChartValueAxis>
+                            <ChartValueAxisItem
+                                title={{
+                                    text: "Percent of Data Rows Valid",
+                                    font,
+                                }}
+                                labels={{ format: "{0:p}" }}
+                            />
+                        </ChartValueAxis>
+                        <ChartCategoryAxis>
+                            <ChartCategoryAxisItem
+                                labels={{
+                                    font,
+                                    step: 15.5,
+                                    skip: 15,
+                                    rotation: "auto",
+                                    padding: [0, 0, 0, 120],
+                                    visible: true,
+                                }}
+                                categories={dateLabels}
+                            />
+                        </ChartCategoryAxis>
+                        <ChartSeries>
+                            <ChartSeriesItem
+                                field="passing"
+                                type="area"
+                                color={"blue"}
+                                data={series}
+                                markers={{ visible: false }}
+                                tooltip={{
+                                    background: "blue",
+                                    visible: true,
+                                    format: "{0}",
+                                }}
+                            />
+                        </ChartSeries>
+                    </Chart>
+                </div>
+            ) : (
+                <>Loading</>
+            )}
         </div>
-      ) : (
-        <>Loading</>
-      )}
-    </div>
-  );
+    );
 }
