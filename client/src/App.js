d0a6b15a01b1f6335872ad9fa95d7866521b851f
Jack Fairfield <Jfairfield@azmag.gov>
2022-09-06 09:46:17 -0700

    Jack (#13)
    * Match old navbar style.  Add routes for future pages.
    
    * formatting

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@arcgis/core/assets/esri/themes/dark/main.css";
 import "@progress/kendo-theme-default/dist/all.css";
+
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/Home";
 import DetectorPage from "./pages/Detector";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import CorridorsPage from "./pages/Corridors";
+import QueryBuilderPage from "./pages/QueryBuilder";
+import AdminPage from "./pages/Admin";
+
 import { DataProvider } from "./stores/DataContext";
 
 function App() {
@@ -15,8 +22,11 @@ function App() {
                 <Router>
                     <Navbar />
                     <Routes>
-                        <Route path="detector/:id" element={<DetectorPage />} />
                         <Route exact path="/" element={<HomePage />} />
+                        <Route path="detector/:id" element={<DetectorPage />} />
+                        <Route path="corridors" element={<CorridorsPage />} />
+                        <Route path="query" element={<QueryBuilderPage />} />
+                        <Route path="admin" element={<AdminPage />} />
                     </Routes>
                 </Router>
             </DataProvider>
--- client/src/components/Map/DetectorMap.js
+++ client/src/components/Map/DetectorMap.js
@@ -1,65 +1,62 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 
-import ArcGISMap from '@arcgis/core/Map';
-import MapView from '@arcgis/core/views/MapView';
-import { whenFalseOnce } from '@arcgis/core/core/reactiveUtils';
-import { useDataStore } from '../../stores/DataContext';
+import ArcGISMap from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import { whenFalseOnce } from "@arcgis/core/core/reactiveUtils";
+import { useDataStore } from "../../stores/DataContext";
 
-import getDetectorsLayer from './getDetectorsLayer';
-let mapLoaded = false;
+import getDetectorsLayer from "./getDetectorsLayer";
 
 function DetectorMap() {
-  const mapDiv = useRef(null);
-  const store = useDataStore();
+    const mapDiv = useRef(null);
+    const store = useDataStore();
 
-  useEffect(() => {
-    if (mapDiv.current && !mapLoaded) {
-      mapLoaded = true;
+    useEffect(() => {
+        if (mapDiv.current) {
+            const _map = new ArcGISMap({
+                basemap: "gray-vector",
+            });
 
-      const _map = new ArcGISMap({
-        basemap: 'gray-vector',
-      });
+            const _view = new MapView({
+                map: _map,
+                container: mapDiv.current,
+                extent: {
+                    spatialReference: {
+                        wkid: 102100,
+                    },
+                    xmax: -12399883.303088231,
+                    xmin: -12546642.690914528,
+                    ymax: 3993315.240863136,
+                    ymin: 3925821.209899271,
+                },
+                constraints: {
+                    rotationEnabled: false,
+                    minZoom: 8,
+                    snapToZoom: true,
+                },
+                popup: {
+                    alignment: "top-center",
+                    dockEnabled: true,
+                    collapseEnabled: false,
+                    actions: [],
+                    dockOptions: {
+                        buttonEnabled: false,
+                        breakpoint: true,
+                    },
+                },
+                ui: { components: [] },
+            });
+            (async () => {
+                const detectorsLayer = await getDetectorsLayer();
+                _map.add(detectorsLayer);
+                store.setDetectorsLayer(detectorsLayer);
+                store.setMap(_map);
+                store.setView(_view);
+            })();
+        }
+    }, [mapDiv, store]);
 
-      const _view = new MapView({
-        map: _map,
-        container: mapDiv.current,
-        extent: {
-          spatialReference: {
-            wkid: 102100,
-          },
-          xmax: -12399883.303088231,
-          xmin: -12546642.690914528,
-          ymax: 3993315.240863136,
-          ymin: 3925821.209899271,
-        },
-        constraints: {
-          rotationEnabled: false,
-          minZoom: 8,
-          snapToZoom: true,
-        },
-        popup: {
-          alignment: 'top-center',
-          dockEnabled: true,
-          collapseEnabled: false,
-          actions: [],
-          dockOptions: {
-            buttonEnabled: false,
-            breakpoint: true,
-          },
-        },
-        ui: { components: [] },
-      });
-      (async () => {
-        const detectorsLayer = await getDetectorsLayer();
-        _map.add(detectorsLayer);
-        store.setDetectorsLayer(detectorsLayer);
-        store.setMap(_map);
-        store.setView(_view);
-      })();
-    }
-  }, [mapDiv, store]);
-
-  return <div className="m-auto h-full w-full" ref={mapDiv}></div>;
+    return <div className="m-auto h-full w-full" ref={mapDiv}></div>;
 }
 
 export default DetectorMap;
