aea5a0c26b1fca7b4f00575913a651bf5a35aec1
Vern Wolfley <vwolfley@users.noreply.github.com>
2022-09-09 11:58:28 -0700

    Detector page update (#14)
    * update dector page
    * update detector page

--- client/src/components/Detector/MiscDetectorData.js
+++ client/src/components/Detector/MiscDetectorData.js
@@ -1,59 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-// import { Card, ListGroup } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Card, ListGroup } from "react-bootstrap";
 
 axios.defaults.withCredentials = true;
 
 export default function MiscDetectorData({ det_num }) {
-  const [data, setData] = useState(null);
-  const dataItems = [
-    {
-      field: 'num_days',
-      label: 'Number of Days in Dataset',
-    },
-    {
-      field: 'gp_lane_cnt',
-      label: 'General Purpose Lanes',
-    },
-    {
-      field: 'hov_lane_cnt',
-      label: 'High-Occupancy Lanes',
-    },
-  ];
-
-  useEffect(() => {
-    (async () => {
-      const res = await axios.get(
-        'http://magdevarcgis/fms/Detector/GetMiscDetectorData',
+    const [data, setData] = useState(null);
+    // console.log(data);
+    const dataItems = [
         {
-          params: {
-            det_num: 50,
-            year: '2021',
-          },
-        }
-      );
-      setData(res.data);
-      console.log(res.data);
-    })();
-  }, [det_num]);
-  return (
-    <div style={{ marginRight: '15px' }}>
-      {data ? (
+            field: "detector_number",
+            label: "Detector Station ID",
+        },
+        { field: "year", label: "Analysis Year" },
+        {
+            field: "num_days",
+            label: "Number of Days in Dataset",
+        },
+        {
+            field: "",
+            label: "Number of Qualified Weekdays in the Data",
+        },
+        {
+            field: "gp_lane_cnt",
+            label: "General Purpose Lanes",
+        },
+        {
+            field: "hov_lane_cnt",
+            label: "High-Occupancy Lanes",
+        },
+    ];
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get(
+                "http://magdevarcgis/fms/Detector/GetMiscDetectorData",
+                {
+                    params: {
+                        det_num: 50,
+                        year: "2021",
+                    },
+                }
+            );
+            setData(res.data);
+            console.log(res.data);
+        })();
+    }, [det_num]);
+    return (
         <div>
-          {/* <Card style={{ width: '18rem' }}>
-            <Card.Header>Misc Data</Card.Header>
-            <ListGroup variant="flush">
-              {dataItems.map((item, i) => (
-                <ListGroup.Item key={i}>
-                  {item.label}: <b>{data[item.field]}</b>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          </Card> */}
+            {data ? (
+                <div>
+                    <Card style={{ width: "25rem" }}>
+                        <Card.Header className="text-lg font-bold">
+                            ADOT Detector Info
+                        </Card.Header>
+                        <ListGroup variant="flush">
+                            {dataItems.map((item, i) => (
+                                <ListGroup.Item
+                                    key={i}
+                                    className="d-flex justify-content-between align-items-start"
+                                >
+                                    {item.label}:{" "}
+                                    <span className="font-bold">
+                                        {data[item.field]}
+                                    </span>
+                                </ListGroup.Item>
+                            ))}
+                        </ListGroup>
+                    </Card>
+                </div>
+            ) : (
+                <>Loading</>
+            )}
         </div>
-      ) : (
-        <>Loading</>
-      )}
-    </div>
-  );
+    );
 }
