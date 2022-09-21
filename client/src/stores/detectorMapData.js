d285430bb5250cf8924ac9ac6e8032377a31e5c4
Jack Fairfield <jfairfield@azmag.gov>
2022-09-21 15:05:36 -0700

    Add reset button.

--- client/src/stores/detectorMapData.js
+++ client/src/stores/detectorMapData.js
@@ -43,13 +43,23 @@ const detectorMapData = {
         this.updateDefExpression();
     },
     labelsVisible: true,
-    toggleLabels() {
-        this.labelsVisible = !this.labelsVisible;
+    toggleLabels(val) {
+        if (val !== null && val !== undefined) {
+            this.labelsVisible = val;
+        } else {
+            this.labelsVisible = !this.labelsVisible;
+        }
+
         this.detectorsLayer.labelsVisible = this.labelsVisible;
     },
     noDataAvailableShown: true,
-    toggleNoDataAvailableShown() {
-        this.noDataAvailableShown = !this.noDataAvailableShown;
+    toggleNoDataAvailableShown(val) {
+        if (val !== null && val !== undefined) {
+            this.noDataAvailableShown = val;
+        } else {
+            this.noDataAvailableShown = !this.noDataAvailableShown;
+        }
+
         if (this.noDataAvailableShown) {
             this.detectorsLayer.renderer.defaultSymbol = {
                 type: "simple-marker",
@@ -66,6 +76,36 @@ const detectorMapData = {
         }
     },
     mapLoaded: false,
+    anyChanges() {
+        if (this.selectedRoute !== "All") {
+            return true;
+        }
+
+        if (this.selectedYear !== 2021) {
+            return true;
+        }
+
+        if (this.selectedDirection !== "All") {
+            return true;
+        }
+
+        if (this.labelsVisible !== true) {
+            return true;
+        }
+
+        if (this.noDataAvailableShown !== true) {
+            return true;
+        }
+
+        return false;
+    },
+    resetMapControls() {
+        this.setSelectedRoute("All");
+        this.setSelectedYear(2021);
+        this.setSelectedDirection("All");
+        this.toggleLabels(true);
+        this.toggleNoDataAvailableShown(true);
+    },
 };
 
 export default detectorMapData;
