a1cf10d5cbd718de56614692061f2cbd191b5675
Jack Fairfield <Jfairfield@azmag.gov>
2023-03-29 08:44:57 -0700

    Jack cleanup (#85)
    * adding export to csv
    
    * Remove old files that weren't being used anymore.

--- client/src/components/Map/getValidityData.js
+++ client/src/components/Map/getValidityData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { apiUrl } from "../../DocConfig";
 async function getValidityData() {
-    const res = await axios.get(apiUrl + "/Home/GetValidityData");
+    const res = await axios.get(apiUrl + "/Detector/GetValidityData");
     return res.data;
 }
 
